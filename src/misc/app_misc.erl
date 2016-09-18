0cb2ae5dd426d50f452cb7c64190f6ffb178b2e1
alex <909423011@qq.com>
2016-09-18 17:38:18 +0800

    add app_misc get_env set_env

--- src/misc/app_misc.erl
+++ src/misc/app_misc.erl
@@ -4,6 +4,8 @@
     backup/0,
     cur_dir/0,
     ebins/0,
+    get_env/1,
+    set_env/2,
     init/0
 ]).
 
@@ -11,11 +13,23 @@ init() ->
     ok.
 
 cur_dir() ->
-    {ok, Dir} = file:get_cwd(),
-    Dir ++ "/".
+    case get_env(cur_dir) of
+        undefined ->
+            {ok, Dir} = file:get_cwd(),
+            D = Dir ++ "/",
+            set_env(cur_dir, D),
+            D;
+        Val -> Val
+    end.
 
 ebins() ->
     cur_dir() ++ "ebin/*.beam".
+
+set_env(Key, Val) ->
+    mochiglobal:put(Key, Val).
+
+get_env(Key) ->
+    mochiglobal:get(Key).
 
 backup() ->
     ok.
